782e14629945d86612da2124e0e5b647859b1e9c
dumbus <dumbusx@gmail.com>
2024-06-21 02:55:13 +0300

    feat: add VKUI styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+import '@vkontakte/vkui/dist/vkui.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
