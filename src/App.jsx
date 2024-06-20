3fb76832367877eb4b78e3aba3f98e1f092e8508
dumbus <dumbusx@gmail.com>
2024-06-21 02:47:39 +0300

    feat: add VK UI shell to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  ConfigProvider,
+  AdaptivityProvider,
+  AppRoot,
+  Div
+} from '@vkontakte/vkui';
 
 import './App.css';
 
@@ -6,9 +12,15 @@ import AudioPlayer from './components/AudioPlayer';
 
 const App = () => {
   return (
-    <div className="container">
-      <AudioPlayer audioSrc="./audio/audio.mp3" />
-    </div>
+    <ConfigProvider>
+      <AdaptivityProvider>
+        <AppRoot>
+          <Div className="container">
+            <AudioPlayer audioSrc="./audio/audio.mp3" />
+          </Div>
+        </AppRoot>
+      </AdaptivityProvider>
+    </ConfigProvider>
   );
 };
 
