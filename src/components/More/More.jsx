c75cdcad8a6b3628395c16c57574b8c257c68e8b
dumbus <dumbusx@gmail.com>
2024-06-22 23:08:19 +0300

    fix: change paddings for More component to fit layout

--- src/components/More/More.jsx
+++ src/components/More/More.jsx
@@ -6,5 +6,9 @@ import { Icon16MoreVertical } from '@vkontakte/icons';
 import './More.css';
 
 export const More = observer(() => {
-  return <Icon16MoreVertical className="player player__more" width={24} />;
+  return (
+    <div className="player player__more">
+      <Icon16MoreVertical className="player player__more_icon" />
+    </div>
+  );
 });
